57c973778ff00cb094f243c37e4d6579a487327d
BrenoBar51 <brenobarcelos9@gmail.com>
2024-01-10 09:45:23 -0300

    conexão com o db

--- src/pages/PageProducts/index.jsx
+++ src/pages/PageProducts/index.jsx
@@ -3,6 +3,7 @@ import { Dropdown } from 'primereact/dropdown';
 import { Checkbox } from 'primereact/checkbox';
 import { RadioButton } from 'primereact/radiobutton';
 import { useEffect, useState } from 'react';
+import { API } from '../../services';
 
 const PageProductsContainer = styled.div`
     padding: 40px 100px;
@@ -32,18 +33,30 @@ const PageProducts = () => {
             value: 3
         },
     ];
+
+    const [categories, setCategories] = useState([]);
+    const [genders, setGenders] = useState([]);
     const [brands, setBrands] = useState([]);
     
-    function getBrands(){
-        fetch('http://localhost:8000/brands')
-        .then(res => res.json())
-        .then(res => {
-            setBrands(res);
-        })
+    async function getBrands(){
+        const response = await API.get('brands');
+        setBrands(response.data);
+    }
+    
+    async function getCategories(){
+        const response = await API.get('categories');
+        setCategories(response.data);
+    }
+    
+    async function getGenders(){
+        const response = await API.get('genders');
+        setGenders(response.data);
     }
 
     useEffect(() => {
         getBrands();
+        getCategories();
+        getGenders();
     }, []);
 
     return(
@@ -51,7 +64,7 @@ const PageProducts = () => {
         <PageProductsContainer>
             <div className='flex justify-content-between align-items-center'>
                 <h6 className='font-normal'>
-                    <b>Resultados para "Tenis"</b> - 389 Produtos</h6>
+                    <b>{'Resultados para "Tenis"'}</b> - 389 Produtos</h6>
                 <div>
                     <h6 className='p-3 border-1 border-round'>
                         <b>Ordenar por mais relevantes:</b>
@@ -75,7 +88,7 @@ const PageProducts = () => {
                         <ul className='list-style-none'>
                             {
                                 brands.map((marca) => (
-                                    <li className='flex gap-3 mb-2'>
+                                    <li key={marca.brand_id} className='flex gap-3 mb-2'>
                                     <Checkbox id={marca.brand_name}/>
                                     <label htmlFor={marca.brand_name}>{marca.brand_name}</label>
                             </li>
@@ -84,37 +97,25 @@ const PageProducts = () => {
                         </ul>
                         <h6 className='mb-2 mt-3'>Categorias</h6>
                         <ul className='list-style-none'>
-                            <li className='flex gap-3 mb-2'>
-                                <Checkbox id='categoria1'/>
-                                <label htmlFor="categoria1">categoria 1</label>
-                            </li>
-                            <li className='flex gap-3 mb-2'>
-                                <Checkbox id='categoria2'/>
-                                <label htmlFor="categoria2">categoria 2</label>
-                            </li>
-                            <li className='flex gap-3 mb-2'>
-                                <Checkbox id='categoria3'/>
-                                <label htmlFor="categoria3">categoria 3</label>
-                            </li>
-                            <li className='flex gap-3 mb-2'>
-                                <Checkbox id='categoria4'/>
-                                <label htmlFor="categoria4">categoria 4</label>
-                            </li>
+                            {
+                                categories.map((categorias) => (
+                                    <li key={categorias.category_id} className='flex gap-3 mb-2'>
+                                    <Checkbox id={categorias.category_name}/>
+                                    <label htmlFor={categorias.category_name}>{categorias.category_name}</label>
+                                    </li>
+                                ))
+                            }
                         </ul>
                         <h6 className='mb-2 mt-3'>Gêneros</h6>
                         <ul className='list-style-none'>
-                            <li className='flex gap-3 mb-2'>
-                                <Checkbox id='genero1'/>
-                                <label htmlFor="genero1">Gênero 1</label>
-                            </li>
-                            <li className='flex gap-3 mb-2'>
-                                <Checkbox id='genero2'/>
-                                <label htmlFor="genero2">Gênero 2</label>
-                            </li>
-                            <li className='flex gap-3 mb-2'>
-                                <Checkbox id='genero3'/>
-                                <label htmlFor="genero3">Gênero 3</label>
-                            </li>
+                            {
+                                genders.map((generos) => (
+                                    <li key={generos.gender_id} className='flex gap-3 mb-2'>
+                                    <Checkbox id={generos.gender_name}/>
+                                    <label htmlFor={generos.gender_name}>{generos.gender_name}</label>
+                                    </li>
+                                ))
+                            }
                         </ul>
                         <h6 className='mb-2 mt-3'>Estado</h6>
                         <ul className='list-style-none'>
