dabd99901ea3aaf2e8710c08449d0213cbbde7ae
BrenoBar51 <brenobarcelos9@gmail.com>
2024-01-10 10:05:38 -0300

    nova função

--- src/pages/PageProducts/index.jsx
+++ src/pages/PageProducts/index.jsx
@@ -1,139 +1,149 @@
-import { styled } from 'styled-components';
-import { Dropdown } from 'primereact/dropdown';
-import { Checkbox } from 'primereact/checkbox';
-import { RadioButton } from 'primereact/radiobutton';
-import { useEffect, useState } from 'react';
-import { API } from '../../services';
+import { styled } from "styled-components";
+import { Dropdown } from "primereact/dropdown";
+import { Checkbox } from "primereact/checkbox";
+import { RadioButton } from "primereact/radiobutton";
+import { useEffect, useState } from "react";
+import { API } from "../../services";
 
 const PageProductsContainer = styled.div`
-    padding: 40px 100px;
-    background-color: #f9f8fe;
+  padding: 40px 100px;
+  background-color: #f9f8fe;
+  font-size: 16px;
+  & h6 {
     font-size: 16px;
-    & h6{
-        font-size: 16px;
-    }
-    & .content{
-        margin-top: 40px;
-    }
-`;//vscode styled-components
+  }
+  & .content {
+    margin-top: 40px;
+  }
+`; //vscode styled-components
 
 const PageProducts = () => {
-    const [ordenacao, setOrdenacao] = useState(1);
-    const tiposDeOrdenacao = [
-        {
-            name: 'Mais relevantes',
-            value: 1
-        },
-        {
-            name: 'Menor valor',
-            value: 2
-        },
-        {
-            name: 'Maior valor',
-            value: 3
-        },
-    ];
+  const [ordenacao, setOrdenacao] = useState(1);
+  const tiposDeOrdenacao = [
+    {
+      name: "Mais relevantes",
+      value: 1,
+    },
+    {
+      name: "Menor valor",
+      value: 2,
+    },
+    {
+      name: "Maior valor",
+      value: 3,
+    },
+  ];
 
-    const [categories, setCategories] = useState([]);
-    const [genders, setGenders] = useState([]);
-    const [brands, setBrands] = useState([]);
-    
-    async function getBrands(){
-        const response = await API.get('brands');
-        setBrands(response.data);
-    }
-    
-    async function getCategories(){
-        const response = await API.get('categories');
-        setCategories(response.data);
-    }
-    
-    async function getGenders(){
-        const response = await API.get('genders');
-        setGenders(response.data);
-    }
+  const [brands, setBrands] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [genders, setGenders] = useState([]);
+  const [filters, setFilters] = useState([]);
 
-    useEffect(() => {
-        getBrands();
-        getCategories();
-        getGenders();
-    }, []);
+  async function getBrands() {
+    const response = await API.get("brands");
+    setBrands(response.data);
+  }
 
-    return(
+  async function getCategories() {
+    const response = await API.get("categories");
+    setCategories(response.data);
+  }
+
+  async function getGenders() {
+    const response = await API.get("genders");
+    setGenders(response.data);
+  }
+
+  function checkSelectItems(e){
+    let isSelected = e.target.checked;
+    let value = e.target.value;
+    setFilters([...filters, value]);
+    console.log(filters);
+  }
+
+  useEffect(() => {
+    getBrands();
+    getCategories();
+    getGenders();
+  }, []);
+
+  return (
     <>
-        <PageProductsContainer>
-            <div className='flex justify-content-between align-items-center'>
-                <h6 className='font-normal'>
-                    <b>{'Resultados para "Tenis"'}</b> - 389 Produtos</h6>
-                <div>
-                    <h6 className='p-3 border-1 border-round'>
-                        <b>Ordenar por mais relevantes:</b>
-                        <Dropdown
-                            value={ordenacao}
-                            options={tiposDeOrdenacao}
-                            optionLabel="name"
-                            optionValue="value"
-                            onChange={e => setOrdenacao(e.value)}
-                            className='border-0 bg-transparent'
-                        />
-                        </h6>
-                </div>
+      <PageProductsContainer>
+        <div className="flex justify-content-between align-items-center">
+          <h6 className="font-normal">
+            <b>{'Resultados para "Tenis"'}</b> - 389 Produtos
+          </h6>
+          <div>
+            <h6 className="p-3 border-1 border-round">
+              <b>Ordenar por mais relevantes:</b>
+              <Dropdown
+                value={ordenacao}
+                options={tiposDeOrdenacao}
+                optionLabel="name"
+                optionValue="value"
+                onChange={(e) => setOrdenacao(e.value)}
+                className="border-0 bg-transparent"
+              />
+            </h6>
+          </div>
+        </div>
+        <div className="content flex gap-3">
+          <div className="w-3">
+            <div className="bg-white p-4 border-round">
+              <h4 className="mb-3">Filtar por:</h4>
+              <hr className="mb-3" />
+              <h6 className="mb-2">Marcas</h6>
+              <ul className="list-style-none">
+                {brands.map((marca) => (
+                  <li key={marca.brand_id} className="flex gap-3 mb-2">
+                    <Checkbox id={marca.brand_name} value={marca.brand_name}
+                    onChange={(e) => checkSelectItems(e)}
+                    checked={filters.includes(marca.brand_name)}
+                    />
+                    <label htmlFor={marca.brand_name}>{marca.brand_name}</label>
+                  </li>
+                ))}
+              </ul>
+              <h6 className="mb-2 mt-3">Categorias</h6>
+              <ul className="list-style-none">
+                {categories.map((categorias) => (
+                  <li key={categorias.category_id} className="flex gap-3 mb-2">
+                    <Checkbox id={categorias.category_name} />
+                    <label htmlFor={categorias.category_name}>
+                      {categorias.category_name}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+              <h6 className="mb-2 mt-3">Gêneros</h6>
+              <ul className="list-style-none">
+                {genders.map((generos) => (
+                  <li key={generos.gender_id} className="flex gap-3 mb-2">
+                    <Checkbox id={generos.gender_name} />
+                    <label htmlFor={generos.gender_name}>
+                      {generos.gender_name}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+              <h6 className="mb-2 mt-3">Estado</h6>
+              <ul className="list-style-none">
+                <li className="flex gap-3 mb-2">
+                  <RadioButton id="novo" checked />
+                  <label htmlFor="novo">Novo</label>
+                </li>
+                <li className="flex gap-3 mb-2">
+                  <RadioButton id="usado" />
+                  <label htmlFor="usado">Usado</label>
+                </li>
+              </ul>
             </div>
-            <div className='content flex gap-3'>
-                <div className='w-3'>
-                    <div className='bg-white p-4 border-round'>
-                        <h4 className='mb-3'>Filtar por:</h4>
-                        <hr className='mb-3' />
-                        <h6 className='mb-2'>Marcas</h6>
-                        <ul className='list-style-none'>
-                            {
-                                brands.map((marca) => (
-                                    <li key={marca.brand_id} className='flex gap-3 mb-2'>
-                                    <Checkbox id={marca.brand_name}/>
-                                    <label htmlFor={marca.brand_name}>{marca.brand_name}</label>
-                            </li>
-                                ))
-                            }
-                        </ul>
-                        <h6 className='mb-2 mt-3'>Categorias</h6>
-                        <ul className='list-style-none'>
-                            {
-                                categories.map((categorias) => (
-                                    <li key={categorias.category_id} className='flex gap-3 mb-2'>
-                                    <Checkbox id={categorias.category_name}/>
-                                    <label htmlFor={categorias.category_name}>{categorias.category_name}</label>
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                        <h6 className='mb-2 mt-3'>Gêneros</h6>
-                        <ul className='list-style-none'>
-                            {
-                                genders.map((generos) => (
-                                    <li key={generos.gender_id} className='flex gap-3 mb-2'>
-                                    <Checkbox id={generos.gender_name}/>
-                                    <label htmlFor={generos.gender_name}>{generos.gender_name}</label>
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                        <h6 className='mb-2 mt-3'>Estado</h6>
-                        <ul className='list-style-none'>
-                            <li className='flex gap-3 mb-2'>
-                                <RadioButton id='novo' checked/>
-                                <label htmlFor="novo">Novo</label>
-                            </li>
-                            <li className='flex gap-3 mb-2'>
-                                <RadioButton id='usado'/>
-                                <label htmlFor="usado">Usado</label>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className='w-9'>Cards</div>
-            </div>
-        </PageProductsContainer>
+          </div>
+          <div className="w-9">Cards</div>
+        </div>
+      </PageProductsContainer>
     </>
-    );
+  )
 }
 export default PageProducts;
